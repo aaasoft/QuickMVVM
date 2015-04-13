023ac63d8c7f407ffd522412fed0a5a977087e06
张鹏 <scbeta@qq.com>
2015-04-13 17:30:11 +0800

    修改程序集版本信息

--- Quick.MVVM/Properties/AssemblyInfo.cs
+++ Quick.MVVM/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Quick.MVVM")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Simple MVVM Framework for WPF.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Quick.MVVM")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © aaasoft 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
