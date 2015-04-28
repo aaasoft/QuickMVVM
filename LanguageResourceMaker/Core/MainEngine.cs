84ed108c1980e8f614b9f130104416cf97d482be
张鹏 <scbeta@qq.com>
2015-04-28 17:48:35 +0800

    让自动翻译能够翻译cs的语言资源文件

--- LanguageResourceMaker/Core/MainEngine.cs
+++ LanguageResourceMaker/Core/MainEngine.cs
@@ -1,6 +1,7 @@
 ﻿using LanguageResourceMaker.Core.FileHandlers;
 using LanguageResourceMaker.Translator;
 using LanguageResourceMaker.Utils;
+using Quick.MVVM.Utils;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -14,12 +15,13 @@ namespace LanguageResourceMaker.Core
     public class MainEngine
     {
         private MainEngineConfig config;
-        Dictionary<String, IFileHandler> fileHandlerDict = new Dictionary<string, IFileHandler>();
+        private Dictionary<String, IFileHandler> fileHandlerDict = new Dictionary<string, IFileHandler>();
+        private List<String> allLanguageFileList = new List<String>();
 
         public MainEngine(MainEngineConfig config)
         {
             this.config = config;
-            fileHandlerDict.Add("*.xaml", new XamlFileHandler(config) { OutputLanguageFileAction = outputLanguageFile});
+            fileHandlerDict.Add("*.xaml", new XamlFileHandler(config) { OutputLanguageFileAction = outputLanguageFile });
             fileHandlerDict.Add("*.cs", new CsFileHandler() { OutputLanguageFileAction = outputLanguageFile });
         }
 
@@ -36,11 +38,11 @@ namespace LanguageResourceMaker.Core
             //输出到语言文件
             String outputFolder = null;
             if (String.IsNullOrEmpty(config.OutputFolder))
-                outputFolder = projectFolder.FullName;
+                outputFolder = Path.Combine(projectFolder.FullName, "View");
             else
-                outputFolder = config.OutputFolder;
+                outputFolder = Path.Combine(config.OutputFolder, projectFolder.Name);
 
-            String abstractOutputFolder = Path.Combine(outputFolder, projectFolder.Name, "Language", "{0}");
+            String abstractOutputFolder = Path.Combine(outputFolder, "Language", "{0}");
             String abstractOutputFileName = outputFileNameWithoutExtension + ".txt";
 
             //写默认语言资源到文件
@@ -49,13 +51,12 @@ namespace LanguageResourceMaker.Core
                 Directory.CreateDirectory(outputFolder);
             String languageFileName = Path.Combine(outputFolder, abstractOutputFileName);
             File.WriteAllText(languageFileName, LanguageUtils.GetToWriteLanguageText(textList), Encoding.UTF8);
+            allLanguageFileList.Add(languageFileName);
         }
 
         private void _Start()
         {
             config.PushLogAction("开始");
-            //所有的语言资源字典，翻译时用
-            Dictionary<String, List<String>> allLanguageResourceDict = new Dictionary<string, List<string>>();
 
             DirectoryInfo di = new DirectoryInfo(config.InputFolder);
             var projectFiles = di.GetFiles("*.csproj", SearchOption.AllDirectories);
@@ -86,25 +87,26 @@ namespace LanguageResourceMaker.Core
                 config.PushLogAction("翻译中。。。");
                 foreach (String language in config.TranslateTarget)
                 {
-                    String[] allLanguageResourceDictKeys = allLanguageResourceDict.Keys.ToArray();
-                    for (int j = 0; j < allLanguageResourceDictKeys.Length; j++)
+                    for (int j = 0; j < allLanguageFileList.Count; j++)
                     {
-                        String abstractFileName = allLanguageResourceDictKeys[j];
-                        String newFullFileName = String.Format(abstractFileName, language);
-
-                        List<String> textList = allLanguageResourceDict[abstractFileName];
+                        String languageFile = allLanguageFileList[j];
+                        String languageFileContent = File.ReadAllText(languageFile);
+                        Dictionary<Int32, String> languageDict = ResourceUtils.GetLanguageResourceDictionary(languageFileContent);
+                        String newFullFileName = Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(languageFile)),
+                            language, Path.GetFileName(languageFile));
                         List<String> newList = new List<string>();
 
-                        foreach (String text in textList)
+                        foreach (Int32 index in languageDict.Keys)
                         {
-                            config.UpdateLogAction(String.Format("正在翻译第[{0}/{1}]个语言文件[{2}]", j + 1, allLanguageResourceDictKeys.Length, newFullFileName));
+                            String text = languageDict[index];
+                            config.UpdateLogAction(String.Format("正在翻译第[{0}/{1}]个语言文件[{2}]为[{3}]", j + 1, allLanguageFileList.Count, newFullFileName, language));
                             String newText = null;
                             do
                             {
                                 newText = config.Translator.Translate(Thread.CurrentThread.CurrentCulture.Name, language, text);
                                 if (newText == null)
                                 {
-                                    config.UpdateLogAction(String.Format("翻译[{0}]中的[{1}]为[{2}]时失败！", abstractFileName, text, language));
+                                    config.UpdateLogAction(String.Format("翻译[{0}]中的[{1}]为[{2}]时失败！", languageFile, text, language));
                                     config.PushLogAction("");
                                     for (int s = 5; s > 0; s--)
                                     {
